5b443ff6d63ce99d02017f521d7e06c098c36de0
Conlan Rios <conlan@gmail.com>
2020-03-31 17:02:38 -0700

    Return back value 0 or 1 based on whether timezone is night or day

--- rutger-van-der-tas/day-night/update.js
+++ rutger-van-der-tas/day-night/update.js
@@ -14,7 +14,7 @@ async function update() {
 	
 	let contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, CONTRACT_ABI, provider);
 
-	// get the currently input latitude / longituded of "Day / Night Coordinates" holder
+	// get the currently input latitude / longitude of "Day / Night Coordinates" holder
 
 	var tokenId = 90; // https://async.art/art/layer/0x6c424c25e9f1fff9642cb5b7750b0db7312c29ad-90 
 
@@ -52,6 +52,18 @@ async function update() {
 	var isDay = ((localHours >= 6) && (localHours < 18)); // Daylight is from 6am to 5:59pm
 
 	console.log("Is Day = " + isDay);
+
+	var layerIds = [84]; // To control the day / night cycle of the painting we control "Day / Night base" on behalf of the owner
+	// https://async.art/art/layer/0x6c424c25e9f1fff9642cb5b7750b0db7312c29ad-84
+
+	var newLayerValues = [isDay ? 0 : 1]; // Value 0 = Day, 1 = Night
+
+	return {
+		layerIds : layerIds,
+		layerValues : newLayerValues
+	}
 }
 
 exports.update = update
+
+update();
