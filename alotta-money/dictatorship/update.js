1585162e0e32376c548155ca77d6fc57620549f6
Conlan Rios <conlan@gmail.com>
2020-04-06 18:12:44 -0700

    Stub in initial script for dictatorship

---
+++ alotta-money/dictatorship/update.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+var fs = require('fs')
+
+async function run() {
+	var layerIds = [];
+	var newLayerValues = [];
+
+	// TODO seasons
+
+	// TODO vehicles
+
+	// TODO cameras
+
+	// TODO windows
+
+	// TODO NPC
+
+	// TODO Sky
+
+	// TOD Poster
+
+	console.log(layerIds);
+	console.log(newLayerValues)
+}
+
+run();
