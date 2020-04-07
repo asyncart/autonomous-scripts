8213989a811f1521785cfa769222bfd5ac700b84
Conlan Rios <conlan@gmail.com>
2020-04-06 18:27:19 -0700

    Take in seasons to account

--- alotta-money/dictatorship/update.js
+++ alotta-money/dictatorship/update.js
@@ -1,11 +1,27 @@
 const axios = require('axios');
 var fs = require('fs')
 
+const DRUG_BUS_VISIBLE_ID = 6;
+const FRAME_STATE_ID = 33;
+
 async function run() {
+	var now = new Date(Date.now());
+
+	var isSunday = (now.getUTCDay() == 0);
+
+	var utcMonth = now.getUTCMonth();
+	var isWinter = ((utcMonth == 12) || (utcMonth == 0) || (utcMonth == 1));
+
 	var layerIds = [];
 	var newLayerValues = [];
 
-	// TODO seasons
+	// seasons
+	layerIds.push(FRAME_STATE_ID)
+	if (isWinter) {
+		newLayerValues.push(1);
+	} else {
+		newLayerValues.push(0);
+	}
 
 	// TODO vehicles
 
@@ -19,8 +35,35 @@ async function run() {
 
 	// TOD Poster
 
+	// Drug Bus State
+	// Drug Bus only visible on Sundays
+	layerIds.push(DRUG_BUS_VISIBLE_ID)
+	if (isSunday) {
+		newLayerValues.push(0);
+	} else {
+		newLayerValues.push(1);
+	}
+
 	console.log(layerIds);
 	console.log(newLayerValues)
+
+	var cachePath = "cache.json"
+	let cache = JSON.parse(fs.readFileSync(cachePath));
+	var controlToken = cache[1];
+
+	for (var i = 0; i < layerIds.length; i++) {
+		var id = layerIds[i];
+		var value = newLayerValues[i];
+
+		var index = id * 3 + 2;
+		controlToken[index] = value;
+	}
+
+	// write cache file
+	let data = JSON.stringify(cache);
+	fs.writeFileSync(cachePath, data);
+
+	console.log("Randomized to " + cachePath);
 }
 
 run();
